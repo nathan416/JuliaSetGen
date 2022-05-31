b12b5283799895a5816660e44b835dd2eac5860d
nathan416 <nathan.flack@gmail.com>
2022-05-31 16:47:13 -0700

    initial commit

---
+++ julia_set_image.py
@@ -0,0 +1,281 @@
+"""Julia set image
+    Class: CPSC 455
+    By: Nathan Flack
+    Version: 1.0
+"""
+from __future__ import division
+
+import logging
+import math
+import time
+
+import matplotlib
+import numpy as np
+import psutil
+from numba import cuda
+from PIL import Image, ImageDraw, ImageFont
+from sympy import *
+from tqdm import tqdm
+import re
+# import cProfile
+# import pstats
+import os
+
+REAL_RANGE_MIN = -1.0
+REAL_RANGE_MAX = 1.0
+IMAG_RANGE_MIN = -1.3
+IMAG_RANGE_MAX = 1.3
+
+IMAGE_WIDTH = 4000
+IMAGE_HEIGHT = (IMAG_RANGE_MAX - IMAG_RANGE_MIN) * (IMAGE_WIDTH) / (REAL_RANGE_MAX - REAL_RANGE_MIN)
+DIVERGENCE_LIMIT = 4000
+
+FRAMES = 32
+DURATION = 80
+ITERATIONS = 90
+
+FILENAME = f'pictures/stills1/still3.png'
+
+LOGGER = logging.getLogger(__name__)
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -43s %(funcName) '
+              '-35s %(lineno) -5d: %(message)s')
+
+# norm = matplotlib.colors.Normalize(vmin=0, vmax=200)
+
+x, y, z, t, a = symbols('x y z t a')
+# examples
+# 1 - x**2 + x**2 / (2 + 4 * x) + 0.7885 * np.e**(a * 1j)
+# 1 - x + x**2 + 0.7885 * np.e**(a * 1j)
+# x**4 + x**3/(x-1) + x**2/(x**3 + 4 *x**2 + 5) - 0.5885 * np.e**(a * 1j)
+# x**4 + x**3/(x-1) + x**2/(x**3 + 4 *x**2 + 5) + 0.755534*math.cos(a) + 0.737292*1j*math.cos(a) - 2*0.737292*1j
+# 2**x + 0.2885 * np.e**(a * 1j)
+# x**2 + 0.355534*math.cos(2*a)-0.337292*1j*math.cos(a)
+# x**4 + x**3 / (x - 1) + x**2 / (x**3 + 4 * x**2 + 5) + 0.377767 * math.sin(a) + 0.368646 * 1j * math.sin(a) - 0.368646 * 1j + 0.377767
+# x**2 + a*.01 - a*.3*1j
+
+
+@cuda.jit('void(complex64, float32)', device=True)
+def iterating_function(x: np.complex64, a: np.float32):
+    """
+    cuda device function
+    takes an 'x' value and 'a' value and returns the calculated result
+
+    Args:
+        x (complex64): variable1
+        a (float32): variable2
+
+    Returns:
+        complex64: result
+    """
+    return x**4 + x**3 / (x - 1) + x**2 / (x**3 + 4 * x**2 + 5) + 0.377767 * math.sin(a) + 0.368646 * 1j * math.sin(a) - 0.368646 * 1j + 0.377767
+
+
+@cuda.jit('void(complex64, int32)', device=True)
+def divergence_tracker(x: np.complex64, divergence: np.int32):
+    """
+    cuda device function
+    checks whether the x value is diverging and increase divergence by 1 if the x value is not diverging
+
+    Args:
+        x (complex64): value to check
+        divergence (int32): current value in divergence
+
+    Returns:
+        int32: new divergence value
+    """
+    if x.real**2 + x.imag**2 < DIVERGENCE_LIMIT:
+        return divergence + 1
+    return divergence
+
+
+@cuda.jit('void(complex64[:,:], int32[:,:], float32, int32)')
+def helper_func5_image(x, divergence, a, iter):
+    """
+    full cuda kernel. compiled by numba cuda jit from python to a gpu compatible function
+
+    Args:
+        x (complex64[][]): array of complex values to iterate
+        divergence (int32[][]): divergence tracking array
+        a (float32): optional variable
+        iter (int32): amount of times to iterate
+    """
+    xstart, ystart = cuda.grid(2)
+    xstride, ystride = cuda.gridsize(2)
+    for k in range(iter):
+        for i in range(xstart, x.shape[0], xstride):
+            for j in range(ystart, x.shape[1], ystride):
+                if(x[i, j].real**2 + x[i, j].imag**2 < DIVERGENCE_LIMIT):
+                    x[i, j] = iterating_function(x[i, j], a)
+                divergence[i, j] = divergence_tracker(x[i, j], divergence[i, j])
+
+
+def plot_julia_set_image(expression: callable, a, iteration_count: int = 100, real_range_min: float = -1.0, real_range_max: float = 1.0,
+                         imag_range_min: float = -1.0, imag_range_max: float = 1.0, image_width=1920, image_height=1080):
+    """
+    takes complex expression, iterates the expression with a set of initial complex points,
+    with each point corresponding to a pixel in the resulting image,
+    and returns the times it was iterated before the value diverged.
+
+    Args:
+        expression (callable): julia set cuda function
+        iteration_count (int): amount of times to iterate
+        seed_count (int): amount of points to iterate and plot
+        real_range_min (float): min range of real part of complex points
+        real_range_max (float): max range of real part of complex points
+        imag_range_min (float): min range of imag part of complex points
+        imag_range_max (float): max range of imag part of complex points
+        image_width (int): array width of point set
+        image_height (int): array height of point set
+    """
+    # start_time = time.perf_counter()
+
+    real_set = np.linspace(real_range_min, real_range_max, int(image_width)).reshape((1, int(image_width)))
+    imag_set = np.linspace(imag_range_max, imag_range_min, int(image_height)).reshape((int(image_height), 1))
+    complex_set = np.array(real_set + 1j * imag_set, dtype=np.complex64)
+
+    divergence = iterations_till_divergence_image(expression, complex_set, a, iteration_count)
+
+    # LOGGER.info(f'time taken plot_julia_set: {time.perf_counter() - start_time}')
+    return divergence
+
+
+def iterations_till_divergence_image(expression: callable, initial_values: np.ndarray, a: float, iteration_count=1000) -> list:
+    """ iterates over the initial values with a function and returns
+        a list of when the modulus of each complex value goes above a value
+
+    Args:
+        expression (ufunc): function of x to be iterated
+        initial_values (np.complex64[][]): array of initial values
+        a (float): optional variable
+        max_iterations (int): amount of iterations
+
+    Returns:
+        list: list of when each value diverges
+    """
+    # start_time = time.perf_counter()
+    divergence_h = np.zeros(initial_values.shape, dtype=np.int32)
+
+    blockdim = (16, 16)
+    griddimx = math.ceil(IMAGE_WIDTH / blockdim[0])
+    griddimy = math.ceil(IMAGE_HEIGHT / blockdim[1])
+    griddim = (griddimx, griddimy)
+
+    iterating_values_d = cuda.to_device(initial_values)
+    divergence_d = cuda.to_device(divergence_h)
+
+    expression[griddim, blockdim](iterating_values_d, divergence_d, a, iteration_count)
+    divergence_h = divergence_d.copy_to_host()
+
+    # LOGGER.info(f'time taken iterations_till_divergence_image: {time.perf_counter() - start_time}')
+    return divergence_h
+
+
+def save_julia_set_over_time_image(file_name: str, expression: str, real_range_min: float = -2.31, real_range_max: float = 2.31, imag_range_min: float = -1.3, imag_range_max: float = 1.3, image_width=1920, image_height=1080, label_image=False, frames=10):
+    """ generates many julia set images with the a variable changing gradually
+        between each image to create a slightly different image.
+        These images are then combined into a gif and saved.
+
+    Args:
+        file_name (str): name of file location to save gif at
+        expression (str): string representation of expression. currently only used for image text so it wont change the julia set for now
+        real_range_min (float, optional): minimum real. Defaults to -2.31.
+        real_range_max (float, optional): maxiimum real. Defaults to 2.31.
+        imag_range_min (float, optional): minimum imaginary. Defaults to -1.3.
+        imag_range_max (float, optional): maximum imaginary. Defaults to 1.3.
+        image_width (int, optional): pixel width of the image. Defaults to 1920.
+        image_height (int, optional): pixel height of the image. Defaults to 1080.
+        label_image (bool, optional): whether to place expression into the image. Defaults to False.
+        frames (int, optional): how many frames to put in gif. Defaults to 10.
+
+    Returns:
+        _type_: _description_
+    """
+    # start_time = time.perf_counter()
+
+    imgs = []
+    for a in np.linspace(0, 2 * np.pi, frames):
+        if psutil.virtual_memory().available < 200000000:
+            break
+        img = plotting_helper(iterating_function, a, 200, real_range_min, real_range_max, imag_range_min, imag_range_max, image_width, image_height, label_image, expression)
+        imgs.append(img)
+
+    img = imgs[0]  # extract first image from iterator
+    img.save(fp=file_name, format='GIF', append_images=imgs[1:],
+             save_all=True, duration=DURATION, loop=0)
+    return imgs
+
+
+def save_julia_set_image(file_name: str, expression: str, real_range_min: float = -2.31, real_range_max: float = 2.31, imag_range_min: float = -1.3, imag_range_max: float = 1.3, image_width=1920, image_height=1080, label_image=False, a=0, iteration_count=200, cmap='gnuplot'):
+    if(not is_safe(expression)):
+        raise ValueError('invalid expression')
+    sympy_func = sympify(expression)
+    lambda_func = lambdify((x, y), sympy_func, 'math')
+    cuda_func = cuda.jit('void(complex64, float32)', device=True)(lambda_func)
+
+    @cuda.jit('void(complex64[:,:], int32[:,:], float32, int32)')
+    def calculate_julia_set(x, divergence, a, iter):
+        xstart, ystart = cuda.grid(2)
+        xstride, ystride = cuda.gridsize(2)
+        for k in range(iter):
+            for i in range(xstart, x.shape[0], xstride):
+                for j in range(ystart, x.shape[1], ystride):
+                    if(x[i, j].real**2 + x[i, j].imag**2 < DIVERGENCE_LIMIT):
+                        x[i, j] = cuda_func(x[i, j], a)
+                    divergence[i, j] = divergence_tracker(x[i, j], divergence[i, j])
+
+    img = plotting_helper(calculate_julia_set, a, iteration_count, real_range_min, real_range_max, imag_range_min, imag_range_max, image_width, image_height, label_image, expression, cmap)
+    return img
+
+def save_img_to_file(image, filename):
+    # background = Image.new("RGB", image.size, (255, 255, 255))
+    # background.paste(image, mask=image.split()[3]) # 3 is the alpha channel
+    image.save(fp=filename, format='PNG')
+
+def plotting_helper(expression: callable, a, iteration_count: int = 100, real_range_min: float = -2.31, real_range_max: float = 2.31, imag_range_min: float = -1.3, imag_range_max: float = 1.3, image_width=1920, image_height=1080, label_image=False, function_string='', cmap='gnuplot'):
+    norm = matplotlib.colors.Normalize(vmin=0, vmax=iteration_count+1)
+    normalized = norm(plot_julia_set_image(expression, a, iteration_count, real_range_min, real_range_max, imag_range_min, imag_range_max, image_width, image_height))
+
+    color_map = matplotlib.cm.get_cmap(cmap)
+    img = Image.fromarray(np.uint8(color_map(normalized) * 255))
+    if(label_image):
+        I1 = ImageDraw.Draw(img)
+        my_font = ImageFont.truetype('arial', 20)
+        I1.text((28, 36), function_string, fill=(255, 0, 0), font=my_font)
+    return img
+
+def _is_invalid(c):
+    return c.isalpha() or c == '_'
+
+def is_safe(inp_string):
+    """ Blacklist attribute access, simply by checking for any period that is
+    not surrounded by numbers. Returns True for '3.4', but not for 'a.b' """
+    # components = inp_string.split(".")
+    after = re.findall(r'(?<=\.)[^\s]', inp_string) # gets the non whitespace on right of period
+    before = re.findall(r'[^\s](?=\.)', inp_string) # gets the non whitespace on left of period
+    components = list(before) + list(after)
+    if len(components) == 1:
+        return True
+    for c in components:
+        if _is_invalid(c):
+            return False
+    return True
+
+def main():
+    # logging.basicConfig(level=logging.DEBUG,
+    #                     format=LOG_FORMAT, filename='log_file_test.log')
+    # logging.getLogger('matplotlib.font_manager').setLevel(logging.WARNING)
+    # logging.getLogger('numba.core').setLevel(logging.WARNING)
+    # logging.getLogger('numba.cuda.cudadrv.driver').setLevel(logging.WARNING)
+
+    
+    # strfunc = 'x**4 + x**3 / (x - 1) + x**2 / (x**3 + 4 * x**2 + 5) + 0.377767 * sin(.5) + 0.368646 * 1j * sin(2.6) - 0.368646 * 1j + 0.377767'
+    # cProfile.run(f'save_julia_set_image(FILENAME, \'{strfunc}\', REAL_RANGE_MIN, REAL_RANGE_MAX, IMAG_RANGE_MIN, IMAG_RANGE_MAX, IMAGE_WIDTH, IMAGE_HEIGHT, True)', 'log_file_test.log') 
+    # save_julia_set_image(f'pictures/sequence/julia{len(os.listdir(os.path.join(os.getcwd(), "pictures/sequence")))+1}.png', strfunc, REAL_RANGE_MIN, REAL_RANGE_MAX, IMAG_RANGE_MIN, IMAG_RANGE_MAX, IMAGE_WIDTH, IMAGE_HEIGHT, True, iteration_count= ITERATIONS,cmap='hsv')
+    # p = pstats.Stats('log_file_test.log')
+    # p.sort_stats('cumulative').print_stats(20)
+
+    ...
+
+if __name__ == '__main__':
+    # multiprocessing.freeze_support()
+    main()
---
+++ main.py
@@ -0,0 +1,202 @@
+
+import io
+import re
+import threading
+from functools import partial
+from os.path import dirname
+from queue import Queue
+from time import sleep
+
+from kivy.config import Config
+Config.set('graphics', 'window_state', 'maximized')
+from julia_set_image import save_julia_set_image, save_img_to_file
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.core.image import Image as CoreImage
+from kivy.logger import Logger
+from kivy.properties import (NumericProperty)
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+
+curdir = dirname(__file__)
+
+
+class JuliaGrid(GridLayout):
+    curr_dir = dirname(__file__)
+    status_box = None
+    status_label = None
+    curr_img = None
+
+    def set_image_texture_property(self, value, *largs):
+        self.ids.image.texture = value.texture
+        
+    def set_status_text_property(self, value, *largs):
+        self.status_label.text = value
+        
+    def set_curr_img_property(self, value, *largs):
+        self.curr_img = value
+    
+    def do_action(self, *args, **kwargs):
+        if 'content' in self.ids:
+            return self.ids.content.add_widget(*args, **kwargs)
+        return super(JuliaGrid, self).add_widget(*args, **kwargs)
+
+    def save_julia_set(self):
+        filename = str(self.ids.filename.text)
+        expr = str(self.ids.expr.text)
+        iterations = self.ids.iterations.input_value
+        cmap = str(self.ids.cmap.text)
+        real_range_min = self.ids.coordinates.x_val - self.ids.zoom.input_value
+        real_range_max = self.ids.coordinates.x_val + self.ids.zoom.input_value
+        imag_range_min = self.ids.coordinates.y_val - self.ids.zoom.input_value * (self.ids.aspect_ratio.y_val / self.ids.aspect_ratio.x_val)
+        imag_range_max = self.ids.coordinates.y_val + self.ids.zoom.input_value * (self.ids.aspect_ratio.y_val / self.ids.aspect_ratio.x_val)
+        image_width = int(self.ids.image_width.input_value)
+        image_height = int(self.ids.image_width.input_value * self.ids.aspect_ratio.y_val / self.ids.aspect_ratio.x_val)
+        filename = filename.replace('/', '\\')
+
+        if image_width * image_height < 178956970:
+            self.curr_img = None
+            save_thread = PlotThread(self, 1, 'save_thread', args=(filename, expr, real_range_min, real_range_max, imag_range_min, imag_range_max, image_width, image_height, False, 0, iterations, cmap))
+            save_thread.start()
+            if self.status_box is None:
+                self.status_box = BoxLayout(size_hint_y=None, height='48dp')
+                self.ids.option_box.add_widget(self.status_box)
+                self.status_label = Label(text='Generating Image')
+                self.status_box.add_widget(self.status_label)
+            else:
+                self.status_label.text = 'Generating Image'
+        sleep(.1)
+
+    def save_to_file(self):
+        filename = str(self.ids.filename.text)
+        img = self.curr_img
+        if img is not None:
+            save_img_to_file(img, filename)
+
+
+class AspectRatioInput(TextInput):
+    x_val = NumericProperty(16)
+    y_val = NumericProperty(9)
+    pat = re.compile('[^0-9]')
+
+    def insert_text(self, substring: str, from_undo=False):
+        pat = self.pat
+        if ':' in self.text:
+            s = re.sub(pat, '', substring)
+        else:
+            s = ':'.join(
+                re.sub(pat, '', s)
+                for s in substring.split(':', 1)
+            )
+
+        ret_val = super().insert_text(s, from_undo=from_undo)
+        split = self.text.split(':', 1)
+        if split[0] != '':
+            self.x_val = int(split[0])
+        if split[-1] != '':
+            self.y_val = int(split[-1])
+        return ret_val
+
+    def keyboard_on_key_up(self, window, keycode):
+        split = self.text.split(':', 1)
+        if split[0] != '':
+            self.x_val = int(split[0])
+        if split[-1] != '':
+            self.y_val = int(split[-1])
+        return super().keyboard_on_key_up(window, keycode)
+
+
+class CoordinateInput(TextInput):
+    x_val = NumericProperty(0.0)
+    y_val = NumericProperty(0.0)
+    pat = re.compile('[^0-9.-]')
+
+    def insert_text(self, substring: str, from_undo=False):
+        pat = self.pat
+        if ',' in self.text:
+            s = re.sub(pat, '', substring)
+        else:
+            s = ','.join(
+                re.sub(pat, '', s)
+                for s in substring.split(',', 1)
+            )
+
+        ret_val = super().insert_text(s, from_undo=from_undo)
+        split = self.text.split(',', 1)
+        if split[0] != '' and split[0] != '.' and len(re.findall(r'[.]', split[0])) < 2:
+            self.x_val = float(split[0])
+        if split[-1] != '' and split[-1] != '.' and len(re.findall(r'[.]', split[-1])) < 2:
+            self.y_val = float(split[-1])
+        return ret_val
+
+    def keyboard_on_key_up(self, window, keycode):
+        split = self.text.split(',', 1)
+        if split[0] != '' and split[0] != '.' and len(re.findall(r'[.]', split[0])) < 2:
+            self.x_val = float(split[0])
+        if split[-1] != '' and split[-1] != '.' and len(re.findall(r'[.]', split[-1])) < 2:
+            self.y_val = float(split[-1])
+        return super().keyboard_on_key_up(window, keycode)
+
+
+class IntInput(TextInput):
+    input_value = NumericProperty(1920)
+
+    def insert_text(self, substring: str, from_undo=False):
+        ret_val = super().insert_text(substring, from_undo=from_undo)
+        if self.text != '':
+            self.input_value = int(self.text)
+        return ret_val
+
+    def keyboard_on_key_up(self, window, keycode):
+        if self.text != '':
+            self.input_value = int(self.text)
+        return super().keyboard_on_key_up(window, keycode)
+
+
+class FloatInput(TextInput):
+    input_value = NumericProperty(1.0)
+
+    def insert_text(self, substring: str, from_undo=False):
+        ret_val = super().insert_text(substring, from_undo=from_undo)
+        if self.text != '' and self.text != '.':
+            self.input_value = float(self.text)
+        return ret_val
+
+    def keyboard_on_key_up(self, window, keycode):
+        if self.text != '' and self.text != '.':
+            self.input_value = float(self.text)
+        return super().keyboard_on_key_up(window, keycode)
+
+
+class JuliaApp(App):
+    def build(self):
+        grid = JuliaGrid(rows=1, cols=2)
+        return grid
+
+
+class PlotThread(threading.Thread):
+    def __init__(self, grid: JuliaGrid, threadID, name, args):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.args = args
+        self.grid = grid
+
+    def run(self):
+        img = save_julia_set_image(*self.args)
+        Clock.schedule_once(partial(self.grid.set_curr_img_property, img))
+        while(self.grid.curr_img is None):
+            ...
+        Clock.schedule_once(partial(self.grid.set_status_text_property, 'Done'))
+        buffer = io.BytesIO()
+        
+        self.grid.curr_img.save(buffer, format='png')
+        buffer.seek(0)
+        im = CoreImage(io.BytesIO(buffer.read()), ext='png')
+        Clock.schedule_once(partial(self.grid.set_image_texture_property, im))
+
+
+if __name__ == '__main__':
+    JuliaApp().run()
